3bc6168f6e13e5f9f54de76a94052a96d5c9fc44
HAMIDI Oussama <oussama.hamidi@ens.uvsq.fr>
2020-10-11 15:23:42 +0100

    test toString

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,13 @@
 public class Fraction {
 
+    double rationnel;
+
+    public Fraction (double rationnel){
+        this.rationnel = rationnel;
+    }
+
+    public String toString(){
+        return String.valueOf(rationnel);
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main( String[] args) {
-
-
-            System.out.println("test");
-
+        Fraction fraction = new Fraction(2.45);
+        System.out.println(fraction.rationnel);
     }
 }
